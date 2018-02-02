150be6805b68ebebb0b82bf9f15068995487794c
rudyknowak <rudyknowak@gmail.com>
2018-02-02 20:27:05 +0100

    do sprawdzenia - poprawka pierwsza

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -2,18 +2,40 @@ import java.util.Scanner;
 
  public class MathQuiz {
 
-      static boolean question1() {
+     public boolean question1() {
+         int userChoise;
          Scanner input = new Scanner(System.in);
-         int userChoise = input.nextInt();
+         System.out.println("Jaki jest wynik mnożenia 3*5?");
+         userChoise = input.nextInt();
 
-         if (userChoise != 15) {
-             System.out.println("Nieprawidłowy wynik");
-             System.out.println("Podaj inną wartość");
-             userChoise = input.nextInt();
-         }else {
-             System.out.println("Prawidłowy wynik");
-             input.close();
+         if (userChoise == 15) {
+             return true;
          }
-        return true;
+         return false;
      }
+
+     public boolean question2(){
+         int userChoise;
+         Scanner input = new Scanner(System.in);
+         System.out.println("Jakie jest pole kwadratu o boku 12");
+         userChoise = input.nextInt();
+
+         if (userChoise == 144) {
+             return true;
+         }
+         return false;
+     }
+
+     public boolean question3(){
+         int userChoise;
+         Scanner input = new Scanner(System.in);
+         System.out.println("Jaki jest pierwiastek kwadratowy z liczby 15129?");
+         userChoise = input.nextInt();
+
+         if (userChoise == 123) {
+             return true;
+         }
+         return false;
+     }
+
  }
--- src/MathQuizTest.java
+++ src/MathQuizTest.java
@@ -1,6 +1,27 @@
 public class MathQuizTest {
+
     public static void main(String[] args) {
-        System.out.println("Jaki jest wynik mnożenia 3*5?");
-        MathQuiz.question1();
+
+        MathQuiz pyt = new MathQuiz();
+        System.out.println("Odpowiedz na pierwsze pytanie");
+        if (pyt.question1()){
+            System.out.println("Prawidłowy wynik");
+        }else {
+            System.out.println("Źle");
+        }
+
+        System.out.println("Odpowiedz na drugie pytanie");
+        if (pyt.question2()){
+            System.out.println("Prawidłowy wynik");
+        }else {
+            System.out.println("Źle");
+        }
+
+        System.out.println("Odpowiedz na trzecie pytanie");
+        if (pyt.question3()){
+            System.out.println("Prawidłowy wynik");
+        }else {
+            System.out.println("Źle");
+        }
     }
 }
